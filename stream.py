a39d64d306fbf84b0f4b835d1810a209fdda2905
Josh <bluedude227@gmail.com>
2018-01-25 13:45:27 -0600

    Changed filestream.sh in RPi-Edge to sleep before starting stream. Stream works now.

--- stream.py
+++ stream.py
@@ -2,26 +2,19 @@ import cv2
 import subprocess
 import sys
 
-# Start video capture before opening rtp connection to avoid keyframe errors
-vcap = cv2.VideoCapture("./pi.sdp")
-'''
-ret, frame = vcap.read()
-
 HOST="pi@192.168.12.4"
 # Ports are handled in ~/.ssh/config since we use OpenSSH
-COMMAND="/home/pi/gits/RPi-Edge/filestream.sh 10.8.0.3 /home/pi/gits/RPi-Edge/video.mp4"
+COMMAND="/home/pi/gits/RPi-Edge/filestream.sh 192.168.12.3 /home/pi/gits/RPi-Edge/video.mp4 y > /dev/null 2>&1 &"
 
+print ("sshing...")
 ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
-                       shell=False,
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-result = ssh.stdout.readlines()
-if result == []:
-    error = ssh.stderr.readlines()
-    print >>sys.stderr, "ERROR: %s" % error
-else:
-    print (result)
-'''
+                       shell=False)
+#result = ssh.stdout.readlines()
+print ("DONE SSHING")
+
+#filestream.sh above is coded to sleep for a second before executing.
+#This is necessary because below command failes if started before stream starts
+vcap = cv2.VideoCapture("./pi.sdp")
 while(1):
     ret, frame = vcap.read()
     cv2.imshow('VIDEO', frame)
