c56accdaf38154bd28d875fe13fa0677da1762ec
Josh <bluedude227@gmail.com>
2018-01-25 13:49:06 -0600

    Added argument parsing for IP address of client. Usage: python ./stream.py 192.168.X.X

--- stream.py
+++ stream.py
@@ -1,10 +1,14 @@
 import cv2
 import subprocess
 import sys
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("clientip", help="IP address of client running stream.py")
+args = parser.parse_args()
 
 HOST="pi@192.168.12.4"
 # Ports are handled in ~/.ssh/config since we use OpenSSH
-COMMAND="/home/pi/gits/RPi-Edge/filestream.sh 192.168.12.3 /home/pi/gits/RPi-Edge/video.mp4 y > /dev/null 2>&1 &"
+COMMAND="/home/pi/gits/RPi-Edge/filestream.sh " + args.clientip + " /home/pi/gits/RPi-Edge/video.mp4 y > /dev/null 2>&1 &"
 
 print ("sshing...")
 ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
